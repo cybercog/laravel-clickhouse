2244cf6783538e45e002c781bc93b1848eb7b355
Flavien HIRTZ <flavien.hirtz@infomaniak.com>
2026-07-03 14:53:48 +0200

    feat: add cluster_name to clickhouse config

--- config/clickhouse.php
+++ config/clickhouse.php
@@ -24,6 +24,7 @@ return [
     */
 
     'connection' => [
+        'cluster_name' => env('CLICKHOUSE_CLUSTER_NAME'),
         'host' => env('CLICKHOUSE_HOST', 'localhost'),
         'port' => env('CLICKHOUSE_PORT', 8123),
         'username' => env('CLICKHOUSE_USER', 'default'),
